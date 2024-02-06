7d6c02c55254bb1251a5dc16df7aaca6e54b1988
nathanielfernandes <nathaniel.s.fernandes@gmail.com>
2024-02-05 23:25:27 -0500

    removed yapp

--- src/lib/Alerts/Alerts.svelte
+++ src/lib/Alerts/Alerts.svelte
@@ -51,47 +51,5 @@
 				></button
 			>
 		</div>
-
-		<!-- <span transition:fade> -->
-		<!-- <Alert class="m-2" color={alert.color}>
-				<span slot="icon"
-					><svg
-						aria-hidden="true"
-						class="w-5 h-5"
-						fill="currentColor"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
-						><path
-							fill-rule="evenodd"
-							d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-							clip-rule="evenodd"
-						/></svg
-					>
-				</span>
-				<span class="items-center flex">
-					<span class="font-medium">{alert.title}</span>
-					{alert.message}
-
-					<button
-						type="button"
-						class=" ml-2 focus:outline-none whitespace-normal rounded-lg focus:ring-2 p-1.5 -mx-1.5 -my-1.5"
-						aria-label="Close"
-						on:click={alert.close}
-						><span class="sr-only">Close</span>
-						<svg
-							class="w-5 h-5"
-							fill="currentColor"
-							viewBox="0 0 20 20"
-							xmlns="http://www.w3.org/2000/svg"
-							><path
-								fill-rule="evenodd"
-								d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-								clip-rule="evenodd"
-							/></svg
-						></button
-					>
-				</span>
-			</Alert> -->
-		<!-- </span> -->
 	{/each}
 </span>
