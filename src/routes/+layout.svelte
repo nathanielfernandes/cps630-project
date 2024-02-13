99a7ee482364418f4cb6412d7af46c53785383aa
Charlie Zhao <yczhao0916@gmail.com>
2024-02-13 10:05:03 -0500

    create basic navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <!-- src/routes/+layout.svelte -->
 <script lang="ts">
 	import '../app.css';
+	import TMULogo from '$lib/images/TMU-rgb.png';
 
 	import { invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
@@ -22,7 +23,204 @@
 
 		return () => subscription.unsubscribe();
 	});
+
+	let showHamburgerMenu = false;
+	let showCategoriesDropdown = false;
 </script>
 
 <Alerts />
+
+<nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+	<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+		<a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+			<img src={TMULogo} class="h-10" alt="TMU Logo" />
+			<span class="self-center text-2xl font-semibold whitespace-nowrap text-black dark:text-white"
+				>Marketplace</span
+			>
+		</a>
+		<button
+			data-collapse-toggle="navbar-multi-level"
+			type="button"
+			class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+			aria-controls="navbar-multi-level"
+			aria-expanded="false"
+			on:click={() => (showHamburgerMenu = !showHamburgerMenu)}
+		>
+			<span class="sr-only">Open main menu</span>
+			<svg
+				class="w-5 h-5"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 17 14"
+			>
+				<path
+					stroke="currentColor"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M1 1h15M1 7h15M1 13h15"
+				/>
+			</svg>
+		</button>
+		<div
+			class:hidden={!showHamburgerMenu}
+			class="w-full md:block md:w-auto"
+			id="navbar-multi-level"
+		>
+			<ul
+				class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+			>
+				<li>
+					<a
+						href="/"
+						class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+						aria-current="page">Home</a
+					>
+				</li>
+				<li>
+					<button
+						id="dropdownNavbarLink"
+						data-dropdown-toggle="dropdownNavbar"
+						class="flex items-center justify-between w-full py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+						on:click={() => (showCategoriesDropdown = !showCategoriesDropdown)}
+						>Categories<svg
+							class="w-2.5 h-2.5 ms-2.5"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 10 6"
+						>
+							<path
+								stroke="currentColor"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="m1 1 4 4 4-4"
+							/>
+						</svg></button
+					>
+					<!-- Dropdown menu -->
+					<div
+						id="dropdownNavbar"
+            class:hidden={!showCategoriesDropdown}
+            class:absolute={showCategoriesDropdown}
+						class="z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+					>
+						<ul
+							class="py-2 text-sm text-gray-700 dark:text-gray-200"
+							aria-labelledby="dropdownLargeButton"
+						>
+							<li>
+								<a
+									href="#"
+									class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+									>Dashboard</a
+								>
+							</li>
+							<li aria-labelledby="dropdownNavbarLink">
+								<button
+									id="doubleDropdownButton"
+									data-dropdown-toggle="doubleDropdown"
+									data-dropdown-placement="right-start"
+									type="button"
+									class="flex items-center justify-between w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+									>Dropdown<svg
+										class="w-2.5 h-2.5 ms-2.5"
+										aria-hidden="true"
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 10 6"
+									>
+										<path
+											stroke="currentColor"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="2"
+											d="m1 1 4 4 4-4"
+										/>
+									</svg></button
+								>
+								<div
+									id="doubleDropdown"
+									class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+								>
+									<ul
+										class="py-2 text-sm text-gray-700 dark:text-gray-200"
+										aria-labelledby="doubleDropdownButton"
+									>
+										<li>
+											<a
+												href="#"
+												class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+												>Overview</a
+											>
+										</li>
+										<li>
+											<a
+												href="#"
+												class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+												>My downloads</a
+											>
+										</li>
+										<li>
+											<a
+												href="#"
+												class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+												>Billing</a
+											>
+										</li>
+										<li>
+											<a
+												href="#"
+												class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+												>Rewards</a
+											>
+										</li>
+									</ul>
+								</div>
+							</li>
+							<li>
+								<a
+									href="#"
+									class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+									>Academic Services</a
+								>
+							</li>
+							<li>
+								<a
+									href="#"
+									class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+									>Items Wanted</a
+								>
+							</li>
+						</ul>
+						<div class="py-1">
+							<a
+								href="#"
+								class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+								>Sign out</a
+							>
+						</div>
+					</div>
+				</li>
+				<li>
+					<a
+						href="#"
+						class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+						>Live Chat</a
+					>
+				</li>
+				<li>
+					<a
+						href="#"
+						class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+						>Contact</a
+					>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
 <slot />
