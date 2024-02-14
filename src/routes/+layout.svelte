201faebc7c044969552a70d6077c503ec75d6136
Charlie Zhao <yczhao0916@gmail.com>
2024-02-14 05:20:48 -0500

    add "Place an Ad" button to navbar when user is logged in

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -76,31 +76,39 @@
 
 <Alerts />
 
-<nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+<nav class="min-w-[32rem] bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
 	<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-		<a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-			<img src={TMULogo} class="h-10" alt="TMU Logo" />
-			<span class="self-center text-2xl font-semibold whitespace-nowrap text-black dark:text-white"
-				>Marketplace</span
-			>
-		</a>
-		<div class="relative flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+    <a href="/" class="shrink-0 flex mr-8 items-center space-x-3 rtl:space-x-reverse">
+      <img src={TMULogo} class="h-10" alt="TMU Logo" />
+      <span class="self-center text-2xl font-semibold whitespace-nowrap text-black dark:text-white"
+        >Marketplace</span
+      >
+    </a>
+		<div class="shrink-0 relative flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
 			{#if session}
-				<button
-					type="button"
-					class="flex items-center text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-					id="user-menu-button"
-					data-dropdown-toggle="user-dropdown"
-					data-dropdown-placement="bottom"
-				>
-					<span class="sr-only">Open user menu</span>
-					<img
-						class="w-10 h-10 rounded-full pointer-events-none"
-						src="/docs/images/people/profile-picture-3.jpg"
-						alt="user photo"
-						on:error={handleProfileImageError}
-					/>
-				</button>
+				<div class="flex gap-5">
+					<button
+						on:click={() => goto('/auth')}
+						type="button"
+						class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+						>Place an Ad</button
+					>
+					<button
+						type="button"
+						class="flex items-center text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+						id="user-menu-button"
+						data-dropdown-toggle="user-dropdown"
+						data-dropdown-placement="bottom"
+					>
+						<span class="sr-only">Open user menu</span>
+						<img
+							class="w-10 h-10 rounded-full pointer-events-none"
+							src="/docs/images/people/profile-picture-3.jpg"
+							alt="user photo"
+							on:error={handleProfileImageError}
+						/>
+					</button>
+				</div>
 				<!-- User dropdown menu -->
 				<div
 					class="absolute hidden z-50 my-4 top-3/4 right-0 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
@@ -148,7 +156,7 @@
 				<button
 					on:click={() => goto('/auth')}
 					type="button"
-					class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+					class="shrink-0 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 					>Sign in / Register</button
 				>
 			{/if}
@@ -156,7 +164,7 @@
 			<button
 				data-collapse-toggle="navbar-cta"
 				type="button"
-				class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+				class="shrink-0 inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
 				aria-controls="navbar-cta"
 				aria-expanded="false"
 				on:click={() => (showHamburgerMenu = !showHamburgerMenu)}
