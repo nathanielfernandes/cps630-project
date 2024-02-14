4b39f201d78b25eecd9ae3cc8a5a58c2d5aeb3cb
Charlie Zhao <yczhao0916@gmail.com>
2024-02-14 06:28:12 -0500

    improve navbar responsiveness

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -76,33 +76,33 @@
 
 <Alerts />
 
-<nav class="min-w-[32rem] bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-	<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="/" class="shrink-0 flex mr-8 items-center space-x-3 rtl:space-x-reverse">
-      <img src={TMULogo} class="h-10" alt="TMU Logo" />
-      <span class="self-center text-2xl font-semibold whitespace-nowrap text-black dark:text-white"
-        >Marketplace</span
-      >
-    </a>
-		<div class="shrink-0 relative flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+<nav class="min-w-96 border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-900">
+	<div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
+		<a href="/" class="mr-8 flex shrink-0 items-center space-x-3 rtl:space-x-reverse">
+			<img src={TMULogo} class="h-10" alt="TMU Logo" />
+			<span class="self-center whitespace-nowrap text-2xl font-semibold text-black dark:text-white"
+				>Marketplace</span
+			>
+		</a>
+		<div class="relative flex shrink-0 space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse">
 			{#if session}
 				<div class="flex gap-5">
 					<button
 						on:click={() => goto('/auth')}
 						type="button"
-						class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+						class="hidden rounded-lg bg-blue-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 sm:block dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 						>Place an Ad</button
 					>
 					<button
 						type="button"
-						class="flex items-center text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+						class="flex items-center rounded-full bg-gray-800 text-sm focus:ring-4 focus:ring-gray-300 md:me-0 dark:focus:ring-gray-600"
 						id="user-menu-button"
 						data-dropdown-toggle="user-dropdown"
 						data-dropdown-placement="bottom"
 					>
 						<span class="sr-only">Open user menu</span>
 						<img
-							class="w-10 h-10 rounded-full pointer-events-none"
+							class="pointer-events-none h-10 w-10 rounded-full"
 							src="/docs/images/people/profile-picture-3.jpg"
 							alt="user photo"
 							on:error={handleProfileImageError}
@@ -111,12 +111,12 @@
 				</div>
 				<!-- User dropdown menu -->
 				<div
-					class="absolute hidden z-50 my-4 top-3/4 right-0 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+					class="absolute right-0 top-3/4 z-50 my-4 hidden list-none divide-y divide-gray-100 rounded-lg bg-white text-base shadow dark:divide-gray-600 dark:bg-gray-700"
 					id="user-dropdown"
 				>
 					<div class="px-4 py-3">
 						<span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-						<span class="block text-sm text-gray-500 truncate dark:text-gray-400"
+						<span class="block truncate text-sm text-gray-500 dark:text-gray-400"
 							>{session.user.email}</span
 						>
 					</div>
@@ -124,39 +124,38 @@
 						<li>
 							<a
 								href="#"
-								class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+								class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
 								>Profile</a
 							>
 						</li>
 						<li>
 							<a
 								href="#"
-								class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+								class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
 								>Order History</a
 							>
 						</li>
 						<li>
 							<a
 								href="#"
-								class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+								class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
 								>Admin Dashboard</a
 							>
 						</li>
-						<li>
-							<a
-								on:click={handleSignOut}
-								href="#"
-								class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-								>Sign out</a
-							>
-						</li>
 					</ul>
+					<div class="py-1">
+						<a
+							on:click={handleSignOut}
+							class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
+							>Sign out</a
+						>
+					</div>
 				</div>
 			{:else}
 				<button
 					on:click={() => goto('/auth')}
 					type="button"
-					class="shrink-0 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+					class="shrink-0 rounded-lg bg-blue-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 					>Sign in / Register</button
 				>
 			{/if}
@@ -164,14 +163,14 @@
 			<button
 				data-collapse-toggle="navbar-cta"
 				type="button"
-				class="shrink-0 inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+				class="inline-flex h-10 w-10 shrink-0 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
 				aria-controls="navbar-cta"
 				aria-expanded="false"
 				on:click={() => (showHamburgerMenu = !showHamburgerMenu)}
 			>
 				<span class="sr-only">Open main menu</span>
 				<svg
-					class="w-5 h-5"
+					class="h-5 w-5"
 					aria-hidden="true"
 					xmlns="http://www.w3.org/2000/svg"
 					fill="none"
@@ -193,23 +192,23 @@
 			id="navbar-multi-level"
 		>
 			<ul
-				class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+				class="mt-4 flex flex-col divide-y divide-gray-100 rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:divide-y-0 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:divide-gray-600 dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900"
 			>
 				<li>
 					<a
 						href="/"
-						class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+						class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
 						class:text-blue-700={$page.url.pathname === '/'}
 						aria-current={$page.url.pathname === '/' ? 'page' : undefined}>Home</a
 					>
 				</li>
-				<li>
+				<li class="relative">
 					<button
 						id="dropdownNavbarLink"
 						data-dropdown-toggle="dropdownNavbar"
-						class="flex items-center justify-between w-full py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+						class="flex w-full items-center justify-between px-3 py-2 text-gray-900 hover:bg-gray-100 md:w-auto md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:focus:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
 						>Categories<svg
-							class="w-2.5 h-2.5 ms-2.5"
+							class="ms-2.5 h-2.5 w-2.5"
 							aria-hidden="true"
 							xmlns="http://www.w3.org/2000/svg"
 							fill="none"
@@ -227,7 +226,7 @@
 					<!-- Dropdown menu -->
 					<div
 						id="dropdownNavbar"
-						class="absolute hidden z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+						class="absolute z-10 left-1/3 md:-left-1/3 top-[125%] hidden w-44 divide-y divide-gray-100 rounded-lg bg-white font-normal shadow dark:divide-gray-600 dark:bg-gray-700"
 					>
 						<ul
 							class="py-2 text-sm text-gray-700 dark:text-gray-200"
@@ -246,9 +245,9 @@
 									data-dropdown-toggle="doubleDropdown"
 									data-dropdown-placement="right-start"
 									type="button"
-									class="flex items-center justify-between w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+									class="flex w-full items-center justify-between px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
 									>Dropdown<svg
-										class="w-2.5 h-2.5 ms-2.5"
+										class="ms-2.5 h-2.5 w-2.5"
 										aria-hidden="true"
 										xmlns="http://www.w3.org/2000/svg"
 										fill="none"
@@ -265,7 +264,7 @@
 								>
 								<div
 									id="doubleDropdown"
-									class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+									class="z-10 hidden w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700"
 								>
 									<ul
 										class="py-2 text-sm text-gray-700 dark:text-gray-200"
@@ -274,28 +273,28 @@
 										<li>
 											<a
 												href="#"
-												class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+												class="block px-4 py-2 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
 												>Overview</a
 											>
 										</li>
 										<li>
 											<a
 												href="#"
-												class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+												class="block px-4 py-2 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
 												>My downloads</a
 											>
 										</li>
 										<li>
 											<a
 												href="#"
-												class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+												class="block px-4 py-2 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
 												>Billing</a
 											>
 										</li>
 										<li>
 											<a
 												href="#"
-												class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+												class="block px-4 py-2 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
 												>Rewards</a
 											>
 										</li>
@@ -327,7 +326,7 @@
 						<div class="py-1">
 							<a
 								href="#"
-								class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+								class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
 								>Sign out</a
 							>
 						</div>
@@ -336,8 +335,16 @@
 				<li>
 					<a
 						href="#"
-						class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+						class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
 						>Chatroom</a
+					>
+				</li>
+				<li class="block flex justify-center px-3 py-2 sm:hidden">
+					<button
+						on:click={() => goto('/auth')}
+						type="button"
+						class="rounded-lg bg-blue-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+						>Place an Ad</button
 					>
 				</li>
 			</ul>
