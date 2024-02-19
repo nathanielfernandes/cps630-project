c418f190869bec87da3f9ed9c6109b30d03ff48e
Charlie Zhao <yczhao0916@gmail.com>
2024-02-19 08:56:00 -0500

    redirect user to homepage if not logged in and show login modal

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,45 +1,46 @@
 // src/hooks.server.ts
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
-import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit'
-import { redirect, type Handle } from '@sveltejs/kit'
-import { sequence } from '@sveltejs/kit/hooks'
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
+import { redirect, type Handle } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
+import { isProtectedPage } from '$lib/protectedPages';
 
 const supabase: Handle = async ({ event, resolve }) => {
-  event.locals.supabase = createSupabaseServerClient({
-    supabaseUrl: PUBLIC_SUPABASE_URL,
-    supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
-    event,
-  })
+	event.locals.supabase = createSupabaseServerClient({
+		supabaseUrl: PUBLIC_SUPABASE_URL,
+		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+		event
+	});
 
-  /**
-   * a little helper that is written for convenience so that instead
-   * of calling `const { data: { session } } = await supabase.auth.getSession()`
-   * you just call this `await getSession()`
-   */
-  event.locals.getSession = async () => {
-    const {
-      data: { session },
-    } = await event.locals.supabase.auth.getSession()
-    return session
-  }
+	/**
+	 * a little helper that is written for convenience so that instead
+	 * of calling `const { data: { session } } = await supabase.auth.getSession()`
+	 * you just call this `await getSession()`
+	 */
+	event.locals.getSession = async () => {
+		const {
+			data: { session }
+		} = await event.locals.supabase.auth.getSession();
+		return session;
+	};
 
-  return resolve(event, {
-    filterSerializedResponseHeaders(name) {
-      return name === 'content-range'
-    },
-  })
-}
+	return resolve(event, {
+		filterSerializedResponseHeaders(name) {
+			return name === 'content-range';
+		}
+	});
+};
 
-const authorization: Handle = async ({event, resolve}) => {
-    // if the route is protected make sure the user is logged in
-    if (event.url.pathname.startsWith("/dashboard") && event.request.method === "GET") {
-        const session = await event.locals.getSession()
-        if (!session) {
-            throw redirect(303, "/login")
-        }
-    }
+const authorization: Handle = async ({ event, resolve }) => {
+	// If the route is protected and user is not logged in, redirect to homepage and show login modal
+	if (isProtectedPage(event.url.pathname) && event.request.method === 'GET') {
+		const session = await event.locals.getSession();
+		if (!session) {
+			throw redirect(303, '/?askLogin=true');
+		}
+	}
 
-    return resolve(event)
-}
+	return resolve(event);
+};
 
-export const handle: Handle = sequence(supabase, authorization)
+export const handle: Handle = sequence(supabase, authorization);
--- src/lib/protectedPages.ts
+++ src/lib/protectedPages.ts
@@ -1,1 +1,7 @@
-export const PROTECTED_PAGES: string[] = ['/dashboard', '/admin'];
+export const PROTECTED_PAGES: string[] = ['dashboard', 'admin'];
+
+const PROTECTED_PAGES_REGEX = new RegExp(`^/(?:${PROTECTED_PAGES.join("|")})(?:/.*)?$`);
+
+export const isProtectedPage = (pathname: string) => {
+  return PROTECTED_PAGES_REGEX.test(pathname);
+}
