1b50095d697386e51d67aa140d1b233c56c0c64b
Maxim Piorischin <maxpiorischin@hotmail.com>
2024-03-29 20:44:12 -0400

    temp semi-working

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -226,7 +226,7 @@
 				</li>
 				<li>
 					<a
-						href="/dashboard/wanted"
+						href="/dashboard/wanted?category=items_wanted"
 						aria-current={$page.url.pathname === '/dashboard/wanted' ? 'page' : undefined}
 						class="block px-3 py-2 text-gray-900 hover:text-blue-500 aria-[current=page]:text-blue-500 md:p-0 dark:text-white dark:hover:text-blue-500"
 						>Wanted Listings</a
