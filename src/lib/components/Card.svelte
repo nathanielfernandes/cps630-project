b53bc57c9b6c72ae304408715b88c494ccb78afe
Sarah Babar <59743057+sarahbabar@users.noreply.github.com>
2024-04-02 21:27:38 -0400

    Refactor card component and add carousel component

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	export let ad_id: number;
 	export let title: string;
 	export let description: string;
 	export let date: string;
@@ -6,37 +7,40 @@
 	export let profile_image: string;
 	export let images: string[];
 	export let user: string;
+	export let category: string;
 </script>
 
-<div class="ad-card w-[300px] place-self-center rounded-lg bg-white shadow-lg">
-	<div class="relative h-[250px] w-[300px] rounded-t-lg">
-		<img src={images[0]} alt={title} class="h-[250px] w-[300px] rounded-t-lg object-cover" />
-		<div
-			class="absolute bottom-3 left-3 flex items-center space-x-2 rounded-lg bg-white bg-opacity-70 p-2"
-		>
-			<img src={profile_image} alt={user} class="h-6 w-6" />
-			<p class="pr-1 text-xs font-medium text-gray-900">{user}</p>
-		</div>
-	</div>
-
-	<div class="p-5">
-		<h2 class="mb-2 line-clamp-1 text-xl font-medium text-gray-900">
-			{title}
-		</h2>
-
-		<p class="mb-4 line-clamp-2 min-h-10 text-sm font-normal text-gray-700">
-			{description}
-		</p>
-
-		<div class="flex items-center justify-between">
-			<p class="text-sm font-medium text-gray-700">${price.toFixed(2)}</p>
-
-			<button
-				class="rounded-lg bg-blue-600 px-3 py-1.5 text-sm font-normal text-white hover:bg-blue-700"
+<a href="/dashboard/{category}/{ad_id}" class="ad-card w-[300px] place-self-center rounded-lg bg-white shadow-lg"> 
+	<div class="ad-card w-[300px] place-self-center rounded-lg bg-white hover:shadow-lg transition duration-200 ease-in-out lg:hover:scale-105">
+		<div class="relative h-[250px] w-[300px] rounded-t-lg">
+			<img src={images[0]} alt={title} class="h-[250px] w-[300px] rounded-t-lg object-cover" />
+			<div
+				class="absolute bottom-3 left-3 flex items-center space-x-2 rounded-lg bg-white bg-opacity-70 p-2"
 			>
-				Contact
-				<i class="fa-solid fa-message ml-2"></i>
-			</button>
+				<img src={profile_image} alt={user} class="h-6 w-6" />
+				<p class="pr-1 text-xs font-medium text-gray-900">{user}</p>
+			</div>
+		</div>
+
+		<div class="p-5">
+			<h2 class="mb-2 line-clamp-1 text-xl font-medium text-gray-900">
+				{title}
+			</h2>
+
+			<p class="mb-4 line-clamp-2 min-h-10 text-sm font-normal text-gray-700">
+				{description}
+			</p>
+
+			<div class="flex items-center justify-between">
+				<p class="text-sm font-medium text-gray-700">${price.toFixed(2)}</p>
+
+				<button
+					class="rounded-lg bg-blue-600 px-3 py-1.5 text-sm font-normal text-white hover:bg-blue-700 transition duration-100 ease-in-out"
+				>
+					Contact
+					<i class="fa-solid fa-message ml-2"></i>
+				</button>
+			</div>
 		</div>
 	</div>
-</div>
+</a> 
