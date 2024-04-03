d9da198ab1f9de0e71c2439419191135cb545bf3
Hazafa Tanveer <hazafa.tanveer123@gmail.com>
2024-04-03 02:57:51 -0400

    login/signup functionality

--- src/lib/components/LoginSignup.svelte
+++ src/lib/components/LoginSignup.svelte
@@ -1,131 +1,183 @@
 <script lang="ts">
-	import Modal from '$lib/Modal/Modal.svelte';
-	import type { SupabaseClient } from '@supabase/supabase-js';
+	import Modal from '$lib/Modal/Modal.svelte'; // modal component
+	import type { SupabaseClient } from '@supabase/supabase-js'; // supabase client
+	import {
+		// alert stores
+		successAlert,
+		errorAlert
+	} from '$lib/Alerts/stores.js';
 
 	export let supabase: SupabaseClient;
-	export let signup = false;
-	export let show = false;
+	export let signup = false; // Whether the user is signing up or logging in
+	export let show = false; // Whether the modal is shown
 
-	let status = '';
 	let email: string;
-	let password: string;
+	let password = '';
 	let resetPW = false;
+	// Password validation
+	$: hasMinLength = password.length >= 7;
+	$: hasLowercase = /[a-z]/.test(password);
+	$: hasUppercase = /[A-Z]/.test(password);
+	$: hasNumber = /\d/.test(password);
+	$: hasSymbol = /\W|_/.test(password);
+	// Overall password validity
+	$: isValidPassword = hasMinLength && hasLowercase && hasUppercase && hasNumber && hasSymbol;
 
-	const handleSignUp = async () => {
-		await supabase.auth.signUp({
-			email,
-			password,
-			options: {
-				emailRedirectTo: `${location.origin}/auth/callback`
-			}
-		});
+	// Handle the authentication operation
+	const handleAuthOperation = async (operationPromise: Promise<any>, successMessage: string) => {
+		const { error } = await operationPromise;
+		if (error) {
+			errorAlert(error);
+			return false;
+		} else {
+			successAlert('Success', successMessage);
+			return true;
+		}
 	};
-
-	const handleSignIn = async () => {
-		await supabase.auth.signInWithPassword({
-			email,
-			password
-		});
-	};
-
-	const resetPassword = async () => {
-		await supabase.auth.resetPasswordForEmail(email);
-	};
-
-	const clearInputs = () => {
-		email = '';
-		password = '';
-		status = '';
-	};
-
-
-	const handleSubmit = async (): Promise<void> => {
-		try {
-			if (signup) {
-				await handleSignUp(); 
-				console.log(email, password);
-			} else {
-				await handleSignIn(); 
-				console.log(email, password);
-			}
-		console.log("Operation was successful");
-		clearInputs();
-
-		} catch (error) {
-			const errorMessage: string = (error instanceof Error) ? error.message : "An unknown error occurred";
-			console.error("An error occurred during the operation:", errorMessage);
-			status = "An error occurred. Please try again.";
-			console.log(status);
+	// Sign up the user
+	const handleSignUp = () => {
+		if (!isValidPassword) {
+			errorAlert('Password requirements not met');
+			return false;
+		} else {
+			return handleAuthOperation(
+				supabase.auth.signUp({
+					email,
+					password,
+					options: { emailRedirectTo: `${location.origin}/auth/callback` }
+				}),
+				'Please check your email to verify your account'
+			);
 		}
 	};
 
+	// Sign in the user
+	const handleSignIn = () => {
+		return handleAuthOperation(
+			supabase.auth.signInWithPassword({ email, password }),
+			'You have successfully logged in'
+		);
+	};
+
+	// Reset the user's password
+	const handleResetPassword = () => {
+		return handleAuthOperation(
+			supabase.auth.resetPasswordForEmail(email),
+			'Please check your email to reset your password'
+		);
+	};
+
+	// Clear the input fields
+	const clearInputs = () => {
+		email = '';
+		password = '';
+	};
+
+	// Handle the form submission
+	const handleSubmit = async (event: Event, close: { (): void }): Promise<void> => {
+		event.preventDefault();
+		let res: boolean;
+		if (resetPW) {
+			res = await handleResetPassword();
+		} else {
+			const handler = signup ? handleSignUp : handleSignIn;
+			res = await handler();
+		}
+
+		if (res) {
+			if (!signup && !resetPW) {
+				close();
+			} else if (resetPW) {
+				resetPW = false;
+			}
+			clearInputs();
+		}
+	};
+	// Set the sign up or login text
 	$: si = signup ? 'Sign Up' : 'Login';
 </script>
 
-<Modal bind:show class="w-96 p-8 space-y-4 text-white" let:close>
-	{@const submit = () => {
-		handleSubmit();
-		//close();
-
-	}}
-	<form class="space-y-4" on:submit={submit}>
+<!-- Login/Signup Modal -->
+<Modal bind:show class="w-96 space-y-4 p-8 text-white" let:close>
+	<form
+		class="space-y-4"
+		on:submit|preventDefault={(e) => {
+			handleSubmit(e, close);
+		}}
+	>
 		{#if !resetPW}
 			<div class="space-y-1">
-				<h1 class="text-white text-xl font-bold">
+				<h1 class="text-xl font-bold text-white">
 					Welcome
 					{#if signup}
 						to the TMU Marketplace
 					{:else}
-						Back 
+						Back
 					{/if}
 				</h1>
-				<p class="text-slate-200 text-sm ">
+				<p class="text-sm text-slate-200">
 					{si} to access the TMU Marketplace
 				</p>
-				{#if status !== ''}
-				<p class="text-red-500 text-sm">{status}</p>
-			{/if}
 			</div>
 			<div class="space-y-1">
-				<p class="text-slate-200 text-sm">Email Address</p>
+				<p class="text-sm text-slate-200">Email Address</p>
 				<input
+					id="username"
 					placeholder="hello@yourcompany.com"
 					type="email"
 					bind:value={email}
-					class="w-full rounded-lg p-2 bg-slate-600"
+					autocomplete="username"
+					class="w-full rounded-lg bg-slate-600 p-2"
 					required
-
 				/>
 			</div>
 			<div class="space-y-1">
-
-				<p class="text-slate-200 text-sm">Password</p>
+				<p class="text-sm text-slate-200">Password</p>
 				<input
 					placeholder="password..."
 					type="password"
 					bind:value={password}
-					class="w-full rounded-lg p-2 bg-slate-600"
+					autocomplete="current-password"
+					class="w-full rounded-lg bg-slate-600 p-2"
 					required
 				/>
-				{#if !signup}			
-				<p class="text-slate-200 text-sm">
+				{#if signup}
+					<p class="text-xs text-slate-200">Password must have:</p>
+					<ul class="list-inside list-disc text-xs">
+						<li class:text-green-500={hasMinLength} class:text-red-500={!hasMinLength}>
+							Minimum 7 characters
+						</li>
+						<li class:text-green-500={hasLowercase} class:text-red-500={!hasLowercase}>
+							At least one lowercase letter
+						</li>
+						<li class:text-green-500={hasUppercase} class:text-red-500={!hasUppercase}>
+							At least one uppercase letter
+						</li>
+						<li class:text-green-500={hasNumber} class:text-red-500={!hasNumber}>
+							At least one number
+						</li>
+						<li class:text-green-500={hasSymbol} class:text-red-500={!hasSymbol}>
+							At least one symbol
+						</li>
+					</ul>
+				{/if}
+				{#if !signup}
 					<button
-						class="text-sm text-blue-700 hover:underline dark:text-blue-500"
+						class=" text-sm text-blue-700 text-slate-200 hover:underline dark:text-blue-500"
 						on:click={() => (resetPW = true)}
 					>
 						Forgot your password?
-				</button>
-				</p>
-			{/if}
+					</button>
+				{/if}
 			</div>
 			<div class="space-y-2">
 				<button
 					type="submit"
-					class="w-full bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
+					class="w-full rounded bg-blue-700 px-4 py-2 font-bold text-white hover:bg-blue-800"
 				>
 					{si}
 				</button>
-				<p class="text-slate-300 text-sm select-none">
+				<p class="select-none text-sm text-slate-300">
 					{#if signup}
 						Already have an account?
 					{:else}
@@ -133,7 +185,10 @@
 					{/if}
 					<button
 						class="text-blue-500 hover:underline"
-						on:click|preventDefault={() => (signup = !signup)}
+						on:click|preventDefault={() => {
+							signup = !signup;
+							clearInputs();
+						}}
 					>
 						{signup ? 'Login' : 'Sign Up'}
 					</button>
@@ -141,30 +196,29 @@
 			</div>
 		{:else}
 			<div class="space-y-1">
-				<h1 class="text-white text-xl font-bold">
-					Reset Password
-				</h1>
-				<p class="text-slate-200 text-sm ">
-					Enter your email address to reset your password
-				</p>
-	
+				<h1 class="text-xl font-bold text-white">Reset Password</h1>
+				<p class="text-sm text-slate-200">Enter your email address to reset your password</p>
 			</div>
-			<p class="text-slate-200 text-sm">Email Address</p>
+			<p class="text-sm text-slate-200">Email Address</p>
 			<input
 				placeholder="hello@yourcompany.com"
 				type="email"
 				bind:value={email}
-				class="w-full rounded-lg p-2 bg-slate-600"
+				class="w-full rounded-lg bg-slate-600 p-2"
 				required
-
 			/>
 			<button
-			class="w-full bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
-			on:click={() => resetPassword()}
-		>
-			Reset Password
-		</button>
-			
+				type="submit"
+				class="w-full rounded bg-blue-700 px-4 py-2 font-bold text-white hover:bg-blue-800"
+			>
+				Reset Password
+			</button>
+			<button
+				class=" text-sm text-blue-700 text-slate-200 hover:underline dark:text-blue-500"
+				on:click={() => (resetPW = false)}
+			>
+				Return to Login
+			</button>
 		{/if}
 	</form>
 </Modal>
