d48e9dfb5811f3bf343143ad7c5473f168c94d16
Sarah Babar <59743057+sarahbabar@users.noreply.github.com>
2024-04-03 23:30:19 -0400

    Refactor Card and page components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,28 +8,28 @@
 
 
 
-<div class="flex justify-center items-center mx-auto my-20 h-[600px] w-full max-w-screen bg-white border border-gray-200" in:fade>
+<div class="lg:flex justify-center items-center mx-auto my-20 h-[600px] w-full max-w-screen bg-white border border-gray-200" in:fade>
 
 	<!-- <div class="mx-auto w-full border-t-4 border-b-4 border-dashed border-blue-600 m-10 bg-transparent p-10"> -->
-	<div class="mx-auto w-full m-10 p-10">
-		<div class="grid grid-cols-2 mx-auto w-full max-w-screen-xl my-20 px-4 content-center ">
+	
+		<div class="md:grid md:grid-cols-2 mx-auto w-full max-w-screen-xl my-20 px-4 content-center ">
 
-			<div class="flex flex-col justify-center">
-				<h1 class="text-gray-900 text-8xl font-semibold">TMU</h1>
-				<h1 class="text-gray-900 text-8xl font-semibold">Marketplace</h1>
-				<p class=" text-blue-600 text-3xl font-semibold mt-8">Student Deals: Shop. Sell. Connect.</p>
+			<div class="text-center md:text-start flex flex-col justify-center">
+				<h1 class="text-4xl text-gray-900 lg:text-6xl xl:text-8xl font-semibold">TMU</h1>
+				<h1 class="text-4xl text-gray-900 lg:text-6xl xl:text-8xl font-semibold">Marketplace</h1>
+				<p class="text-xl text-blue-600 lg:text-2xl xl:text-3xl font-semibold mt-6">Student Deals: Shop. Sell. Connect.</p>
 
 
 			</div>
 		
-			<div class="flex justify-end items-center">
+			<div class="justify-center flex md:justify-end items-center">
 				<img src="/college_students.svg" alt="students" class="h-96" />
 			</div> 
 
 		</div>
 
 		
-	</div>
+	
 	
 
 </div>
