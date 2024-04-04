14e06a8912824535554361bb11af62774789de9f
nathanielfernandes <nathaniel.s.fernandes@gmail.com>
2024-04-03 23:08:21 -0400

    fix inputs

--- src/routes/dashboard/create/[type]/+page.svelte
+++ src/routes/dashboard/create/[type]/+page.svelte
@@ -186,6 +186,8 @@
 							bind:value={price}
 							class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
 							placeholder="Sale Price"
+							min="0"
+							max="999999"
 							required
 						/>
 					</div>
@@ -199,7 +201,7 @@
 							type="text"
 							id="location"
 							class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-							placeholder="Postal Code"
+							placeholder="Location"
 							maxlength="255"
 							required
 						/>
